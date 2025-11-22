a5a3f3b1eca87da0d96e35ab863ff9d46d6a020f
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-21 22:12:44 -0500

    Subir ejercicio 10

---
+++ js/ejercicio10.js
@@ -0,0 +1,11 @@
+const cuadro = document.getElementById("cuadro");
+const btnMover = document.getElementById("btnMover");
+const btnReset = document.getElementById("btnResetear");
+
+btnMover.addEventListener("click", () => {
+    cuadro.classList.add("mover");
+});
+
+btnReset.addEventListener("click", () => {
+    cuadro.classList.remove("mover");
+});
