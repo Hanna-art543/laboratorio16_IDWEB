e70939102eb775c953a3ca2292df293098c8a541
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-21 22:26:18 -0500

    Subir ejercicio 12

---
+++ js/ejercicio12.js
@@ -0,0 +1,11 @@
+//Objeto persona
+const persona = {
+    nombre: "Ana",
+    edad: 20,
+    ciudad: "Lima"
+};
+
+//Convertir a json
+const personaJson = JSON.stringify(persona);
+
+console.log(personaJson);
